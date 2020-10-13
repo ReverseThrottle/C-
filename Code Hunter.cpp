73fca1ecdd9b196a4ba96c55efc94b627491f9cf
ReverseThrottle <54719088+ReverseThrottle@users.noreply.github.com>
2020-10-13 14:37:08 -0700

    Update Code Hunter.cpp

--- Code Hunter.cpp
+++ Code Hunter.cpp
@@ -1,3 +1,11 @@
+/*
+	Zachary Hopping
+	Code Hunter
+	Find errors in the code 
+	Comment on every line and where the errors were located
+	Errors can be indicated by <----------------------------------------------------
+*/
+
 #include <iostream>
 #include <string>
 
@@ -6,6 +14,7 @@ using namespace std;
 
 int main()
 {
+	//Declaring variables and init them to zero
 	string textToAnalyze;
 	int foo = 0;
 	int vowels = 0;
@@ -19,8 +28,11 @@ int main()
 
 	cout << "Welcome to the CIA code Hunter Program!" << endl;
 	cout << "Please type in text to analyze: " << endl;
+
+	//takes user input and stores it in textToAnalyze
 	getline(cin, textToAnalyze);
 
+	//for statement nested with an if statement to check for vowels, consonants, digits, whitespace, and unknow char
 	for (int i = 0; i < textToAnalyze.length(); ++i)
 	{
 		if (textToAnalyze[i] == 'a' || textToAnalyze[i] == 'e' || textToAnalyze[i] == 'i' ||
@@ -28,11 +40,11 @@ int main()
 			textToAnalyze[i] == 'E' || textToAnalyze[i] == 'I' || textToAnalyze[i] == 'O' ||
 			textToAnalyze[i] == 'U')
 		{
-			--vowels;
+			++vowels; //Should be incrementing not decrementing <----------------------------------------------------
 		}
 		else if ((textToAnalyze[i] >= 'a' && textToAnalyze[i] <= 'z') || (textToAnalyze[i] >= 'A' && textToAnalyze[i] <= 'Z'))
 		{
-			//++consonants;
+			++consonants; //commented out consonants <-----------------------------------------------------------------
 		}
 		else if (textToAnalyze[i] >= '0' && textToAnalyze[i] <= '9')
 		{
@@ -48,7 +60,10 @@ int main()
 		}
 	}
 
+	//Sets variable = to user input which is used later for the checksum 
 	lengthOfStringSubmittedForAnalysis = textToAnalyze.length();
+
+	//checksum adds all values to make sure it equals the user input which verifies analysis
 	checkSum = unknownCharacters + vowels + consonants + digits + spaces;
 
 	cout << "Vowels: " << vowels << endl;
@@ -59,6 +74,7 @@ int main()
 	cout << "Number of characters CodeHunter could not identify: " << unknownCharacters << endl;
 	cout << "Checksum: " << checkSum << endl;
 
+	//if statement for checksum 
 	if (checkSum == lengthOfStringSubmittedForAnalysis)
 	{
 		cout << "This program self checking has found this Analysis to be valid." << endl;
@@ -68,6 +84,7 @@ int main()
 		cout << "WARNING! *** This program self checking has found this Analysis to be invalid! Do not use this data!" << endl;
 	}
 
+	//waits for user input before program stops
 	system("pause");
 
 	return 0;
