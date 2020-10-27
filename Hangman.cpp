076d2059b141b0d4e54bcd773fd66f46eb75d362
ReverseThrottle <54719088+ReverseThrottle@users.noreply.github.com>
2020-10-27 14:42:51 -0700

    Create Hangman.cpp

---
+++ Hangman.cpp
@@ -0,0 +1,19 @@
+/*
+	Zachary Hopping
+	10-27-2020
+	Interactive Fiction 
+	Create an interactive fiction game/story using more than one function our code must do something and be interactive
+*/
+
+#include <iostream>
+#include <string>
+#include "InteractiveFunctions.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to Hangman are you ready to play?" << endl;
+	cout << "You can guess the word or one letter at a time" << endl;
+
+}
