382926566d2ed04030b091adccdd119fc225e2cd
ReverseThrottle <54719088+ReverseThrottle@users.noreply.github.com>
2020-12-01 14:30:21 -0700

    Create InteractiveFicHeader.h

---
+++ Header&Source.cpp/InteractiveFicHeader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include<iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string gameExplainfunc(string read);
+string nameFunc(string Username);
+int story1(int options);
