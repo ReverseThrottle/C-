a9a94e7a36d9ce8281d2fe3f3f9e838f73c94c62
ReverseThrottle <54719088+ReverseThrottle@users.noreply.github.com>
2022-06-16 10:53:37 -0700

    Update CH3-Exercise-GetProcInfo2.cpp

--- Win10-SysProgramming/CH3-Exercise-GetProcInfo2.cpp
+++ Win10-SysProgramming/CH3-Exercise-GetProcInfo2.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <psapi.h>
 #include <tlhelp32.h>
+#include <winnt.h>
 #include <processthreadsapi.h>
 #ifndef PCH_H
 #define PCH_H
@@ -19,47 +20,49 @@
 //ThreadLoopHelper - https://docs.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
 //ModuleLoopHelper
 
-bool EnableDebugPrivilege() {
-    HANDLE hToken = NULL;
-    LUID PrivRequired;
-    LPWSTR PrivName = NULL;
-    DWORD cName = NULL;
+bool EnableDebugPrivilege() 
+{
+    HANDLE hToken;
+    LPCTSTR lpszPrivilege = SE_DEBUG_NAME;
+    BOOL bEnablePrivilege = NULL;
+    TOKEN_PRIVILEGES tp;
+    LUID luid;
 
-    if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)); {
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
+    {
 
-        printf("Handle to token: 0x%08x\n", hToken);
-
-        if (LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &PrivRequired))
+        if (!LookupPrivilegeValue(NULL, lpszPrivilege, &luid))
         {
-            //LookupPrivilegeName(NULL, &PrivRequired, NULL, &cName);
-
-            //PrivName = (LPWSTR)sizeof(cName);
-
-            //if (LookupPrivilegeName(NULL, &PrivRequired, PrivName, &cName))
-            //{
-            //    printf("Privileges: %p", PrivName);
-            //}
-            //
-            //printf("Error getting privilege name - Error code: %d\n", GetLastError());
-
-            //Do Something...
-
-
+            printf("LookupPrivilegeValue error: %u\n", GetLastError());
+            return FALSE;
         }
-        else {
-            printf("Error getting priv LUID - Error code : % d\n", GetLastError());
+
+        tp.PrivilegeCount = 1;
+        tp.Privileges[0].Luid = luid;
+        tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+
+        // Enable the privilege or disable all privileges.
+
+        if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES)NULL, (PDWORD)NULL))
+        {
+            printf("AdjustTokenPrivileges error: %u\n", GetLastError());
+            return FALSE;
         }
-        
+
+        if (GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+
+        {
+            printf("The token does not have the specified privilege. \n");
+            return FALSE;
+        }
     }
-
-    CloseHandle(hToken);
-    
-
-
-    return ::GetLastError() == ERROR_SUCCESS;
+    return TRUE;
 }
 
-int main(void) {
+
+
+void ProcNamePriority() {
 
     HANDLE hProcessSnap;
     PROCESSENTRY32 pe32;
@@ -70,11 +73,6 @@ int main(void) {
     //BOOL test;
     DWORD dwPriorityClass;
 
-    if (!EnableDebugPrivilege()) {
-        printf("Failed to enable Debug privilege!\n");
-    }
-
-
     hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (hProcessSnap == INVALID_HANDLE_VALUE)
     {
@@ -83,12 +81,12 @@ int main(void) {
 
     pe32.dwSize = sizeof(PROCESSENTRY32);
 
-    
+
     if (!Process32First(hProcessSnap, &pe32))
     {
         printf("Failed to get first process %d\n", GetLastError());
     }
-    
+
 
     do
     {
@@ -101,19 +99,9 @@ int main(void) {
         }
         else
         {
-            /*test = GetProcessInformation(hProcess, pic, ProcInfo, ProcInfoSize);
-            if (test == 0)
-            {
-                printf("Error retriving info: %d\n", GetLastError());
-            }
-            else
-            {
-                printf("Protection level: %d", ProtectedLevel.ProtectionLevel);
-
-            }*/
             printf("Process ID: %d -- Executable Filename: %ws\n", pe32.th32ProcessID, pe32.szExeFile);
 
-            WCHAR fullPathExe [MAX_PATH];
+            WCHAR fullPathExe[MAX_PATH];
             DWORD pdwSize = MAX_PATH;
             if (!QueryFullProcessImageName(hProcess, 0, fullPathExe, &pdwSize))
             {
@@ -122,7 +110,7 @@ int main(void) {
             }
 
             printf("Full executable path: %ws\n", &fullPathExe);
-            
+
             dwPriorityClass = GetPriorityClass(hProcess);
             if (dwPriorityClass == 0)
             {
@@ -130,15 +118,25 @@ int main(void) {
             }
 
             printf("Class priority: 0x%X\n\n", dwPriorityClass);
-
-
         }
-        
-        
     } while (Process32Next(hProcessSnap, &pe32));
-    
+
+
+}
+
+int main(void) {
+
+
+
+    if (EnableDebugPrivilege() == TRUE) 
+    {
+        printf("Enabled Debug privileges!\n");
+    }
+
+    ProcNamePriority();
 
     
+
 
     getchar();
 
